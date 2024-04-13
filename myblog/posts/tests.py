d7cd54d59d0c788a79fd13d9c6eb2f46412236c1
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2024-04-13 16:24:34 +0100

    TEST:RED - String representation of self

--- myblog/posts/tests.py
+++ myblog/posts/tests.py
@@ -5,3 +5,7 @@ class PostModelTest(TestCase):
     def test_post_model_exists(self):
         posts = Post.objects.all()
         self.assertEqual(len(posts), 0)
+
+    def test_string_representation(self):
+        post = Post(title="My Post")
+        self.assertEqual(str(post), "My Post")
