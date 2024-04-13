06e31143f79608b45249de793da5633fda98098c
Bas Man <73193602+bas-man-dev@users.noreply.github.com>
2024-04-13 19:28:19 +0100

    TEST:GREEN - home view exists

--- myblog/posts/tests.py
+++ myblog/posts/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from . models import Post
+from http import HTTPStatus
 # Create your tests here.
 class PostModelTest(TestCase):
     def test_post_model_exists(self):
@@ -9,3 +10,12 @@ class PostModelTest(TestCase):
     def test_string_representation(self):
         post = Post(title="My Post")
         self.assertEqual(str(post), "My Post")
+
+class HomeViewTest(TestCase):
+    def setUp(self) -> None:
+        Post.objects.create(title="My  First Post", body="My Post Body")
+        Post.objects.create(title="My  Second Post", body="My Second Post Body")
+
+    def test_home_view_exists(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
--- myblog/posts/views.py
+++ myblog/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
+from posts.models import Post
 
 # Create your views here.
+
+class PostView(View):
+    def get(self, request):
+        posts = Post.objects.all()
+        context = {'posts': posts}
+        return render(request, 'posts/index.html', context)
